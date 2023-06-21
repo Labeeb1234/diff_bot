a748bbcae2f68b4ae597ab85c22f1995f455ec27
Labeeb Nassar <75616644+Labeeb1234@users.noreply.github.com>
2023-06-21 22:50:28 +0530

    Add files via upload

--- diff_bot_description/launch/gazebo.launch.py
+++ diff_bot_description/launch/gazebo.launch.py
@@ -12,7 +12,7 @@ from ament_index_python.packages import get_package_share_directory
 def generate_launch_description():
     share_dir = get_package_share_directory('diff_bot_description')
 
-    xacro_file = os.path.join(share_dir, 'urdf', 'diff_bot.xacro')
+    xacro_file = os.path.join(share_dir, 'urdf', 'diff_bot.urdf.xacro')
     robot_description_config = xacro.process_file(xacro_file)
     robot_urdf = robot_description_config.toxml()
 
@@ -63,6 +63,8 @@ def generate_launch_description():
         ],
         output='screen'
     )
+
+    
 
     return LaunchDescription([
         robot_state_publisher_node,
